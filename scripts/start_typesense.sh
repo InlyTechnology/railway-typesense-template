33aba727fccda42d90063cd3b5bf71e529cbe1d6
Anton Hedling <anton.hedling@inly.se>
2025-05-16 14:24:04 +0200

    Switch data dir

--- scripts/start_typesense.sh
+++ scripts/start_typesense.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-exec /opt/typesense-server --api-address 127.0.0.1 --api-port 8118 --enable-search-analytics=true --analytics-flush-interval=300 --analytics-dir=/opt/typesense/analytics-data
+# Use TYPESENSE_DATA_DIR if set, otherwise default to /opt/typesense/data
+DATA_DIR="${TYPESENSE_DATA_DIR:-/opt/typesense/data}"
+
+exec /opt/typesense-server \
+    --api-address 127.0.0.1 \
+    --api-port 8118 \
+    --enable-search-analytics=true \
+    --analytics-flush-interval=300 \
+    --analytics-dir="${DATA_DIR}/analytics"
