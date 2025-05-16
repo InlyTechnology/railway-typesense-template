5bd4bc917994a6097e46a16cda826f29ead0828c
Anton Hedling <anton.hedling@inly.se>
2025-05-16 15:06:12 +0200

    Fix peering adress?

--- scripts/start_typesense.sh
+++ scripts/start_typesense.sh
@@ -6,6 +6,7 @@ DATA_DIR="${TYPESENSE_DATA_DIR:-/opt/typesense/data}"
 exec /opt/typesense-server \
     --api-address 127.0.0.1 \
     --api-port 8118 \
+    --peering-address 127.0.0.1 \
     --enable-search-analytics=true \
     --analytics-flush-interval=300 \
     --analytics-dir="${DATA_DIR}/analytics"
